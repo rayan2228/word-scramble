af00602386935f10957658c5dee78c916765750f
rayan <rhrayan2228@gmail.com>
2023-06-25 12:02:31 +0600

    shuffle word

--- js/script.js
+++ js/script.js
@@ -1,14 +1,23 @@
 const wordElm = document.querySelector(".word");
 const hintElm = document.querySelector(".hint").lastChild;
-const showInUi = (pickedWord) => {
-  let randomWord = pickedWord.word;
-  let randomHint = pickedWord.hint;
+const shuffleWord = (pickedWord) => {
+  let wordArr = pickedWord.split("");
+  for (let index = wordArr.length - 1; index >= 0; index--) {
+    let j = Math.floor(Math.random() * (index + 1));
+    [wordArr[index], wordArr[j]] = [wordArr[j], wordArr[index]];
+  }
+  let finalWord = wordArr.join("");
+  return finalWord;
+};
+const showInUi = (pickedWordAndHint) => {
+  let randomWord = shuffleWord(pickedWordAndHint.word);
+  let randomHint = pickedWordAndHint.hint;
   wordElm.innerText = randomWord;
   hintElm.innerText = randomHint;
 };
 const initialize = () => {
   let randomNumber = Math.floor(Math.random() * words.length);
-  let pickedWord = words[randomNumber];
-  showInUi(pickedWord);
+  let pickedWordAndHint = words[randomNumber];
+  showInUi(pickedWordAndHint);
 };
 initialize();
