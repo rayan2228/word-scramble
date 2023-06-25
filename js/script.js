3d74f65136f29bf9a555365291346fda247e3472
rayan <rhrayan2228@gmail.com>
2023-06-25 21:07:45 +0600

    game done just need to add time

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const wordElm = document.querySelector(".word");
 const hintElm = document.querySelector(".hint").lastChild;
 const wordInputElm = document.querySelector(".wordInput");
+const infoElm = document.querySelector(".info");
 const refreshWordElm = document.querySelector(".refreshWord");
 const checkWordElm = document.querySelector(".checkWord");
 const shuffleWord = (pickedWord) => {
@@ -19,17 +20,34 @@ const showInUi = (pickedWordAndHint) => {
   wordElm.innerText = randomWord;
   hintElm.innerText = randomHint;
 };
+const checkWord = () => {
+  let userWord = wordInputElm.value.toLowerCase();
+  if (userWord) {
+    if (userWord === correctWord) {
+      infoElm.style.color = "#A2FF86";
+      infoElm.style.display = "block";
+      infoElm.innerText = "correct answer.keep playing";
+      wordInputElm.value = "";
+      setTimeout(() => {
+        initialize();
+      }, 1000);
+    }
+  } else {
+    infoElm.style.color = "#F24C3D";
+    infoElm.style.display = "block";
+    infoElm.innerText = "please enter the correct word";
+  }
+};
+const inputValueCheck = () => {
+  infoElm.style.display = "none";
+};
 const initialize = () => {
   let randomNumber = Math.floor(Math.random() * words.length);
   let pickedWordAndHint = words[randomNumber];
   showInUi(pickedWordAndHint);
-};
-initialize();
-const checkWord = () => {
-    let userWord = wordInputElm.value.toLowerCase();
-    if (userWord === correctWord) {
-        console.log("done");
-    }
+  inputValueCheck();
 };
 refreshWordElm.addEventListener("click", initialize);
 checkWordElm.addEventListener("click", checkWord);
+wordInputElm.addEventListener("keyup", inputValueCheck);
+initialize();
