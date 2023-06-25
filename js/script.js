faaf1e0b129c30e22f8398c190121979c8536d60
rayan <rhrayan2228@gmail.com>
2023-06-25 12:25:48 +0600

    check value

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 const wordElm = document.querySelector(".word");
 const hintElm = document.querySelector(".hint").lastChild;
+const wordInputElm = document.querySelector(".wordInput");
+const refreshWordElm = document.querySelector(".refreshWord");
+const checkWordElm = document.querySelector(".checkWord");
 const shuffleWord = (pickedWord) => {
   let wordArr = pickedWord.split("");
   for (let index = wordArr.length - 1; index >= 0; index--) {
@@ -7,6 +10,7 @@ const shuffleWord = (pickedWord) => {
     [wordArr[index], wordArr[j]] = [wordArr[j], wordArr[index]];
   }
   let finalWord = wordArr.join("");
+  correctWord = pickedWord.toLowerCase();
   return finalWord;
 };
 const showInUi = (pickedWordAndHint) => {
@@ -21,3 +25,11 @@ const initialize = () => {
   showInUi(pickedWordAndHint);
 };
 initialize();
+const checkWord = () => {
+    let userWord = wordInputElm.value.toLowerCase();
+    if (userWord === correctWord) {
+        console.log("done");
+    }
+};
+refreshWordElm.addEventListener("click", initialize);
+checkWordElm.addEventListener("click", checkWord);
